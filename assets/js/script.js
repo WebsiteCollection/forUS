590aa69d0a555f79e91774988cadab220dca5fab
EmeraldLiu <1309810@c126-24.ad.ccsd.net>
2017-04-04 11:42:31 -0700

    clean script js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-//Navigation//
+
   $(document).ready(function() {
 
     //-----------NAVIGATION------------//
@@ -95,76 +95,3 @@
 
       //-----------END OF PRESIDENTS------------//
   });
-
-/*
-//Back to Top //
-$(document).ready(function(){
-  // browser window scroll (in pixels) after which the "back to top" link is shown
-  var offset = 500,
-    //duration of the top scrolling animation (in ms)
-    scroll_top_duration = 700,
-    //grab the "back to top" link
-    $back_to_top = $('.cd-top');
-
-  //hide or show the "back to top" link
-  $(window).scroll(function(){
-    ( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
-  });
-
-  //smooth scroll to top
-  $back_to_top.on('click', function(event){
-    event.preventDefault();
-    $('body,html').animate({
-      scrollTop: 0 ,
-      }, scroll_top_duration
-    );
-  });
-
-});
-
-// Cases scroll //
-
-$('.sec-1').click(
-    function (e) {
-      $('html, body').animate({scrollTop: $('#case-1').position().top}, 600);
-      return false;
-    }
-  );
-
-$('.sec-2').click(
-    function (e) {
-      $('html, body').animate({scrollTop: $('#case-2').position().top}, 1200);
-      return false;
-    }
-  );
-
-$('.sec-3').click(
-    function (e) {
-      $('html, body').animate({scrollTop: $('#case-3').position().top}, 1200);
-      return false;
-    }
-  );
-
-
-// President page //
-$(document).ready(function(){
-
-  $('div.presInfo').each(function() {
-    var $presInfo = $(this);
-
-    $("img.presPic", $presInfo).click(function(e) {
-      e.preventDefault();
-      $currentInfo = $("div.info-container", $presInfo);
-      $currentInfo.slideToggle(1300);
-      $("div.info-container").not($currentInfo).hide();
-      return false;
-    });
-
-});
-    
-  $('html').click(function(){
-    $("div.info-container").hide();
-  });
-     
-});
-*/
